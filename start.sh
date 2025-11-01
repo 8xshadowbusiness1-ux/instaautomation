dcfc21f23272ee2c11c11f221bbe539486066cf5
8xshadowbusiness1-ux <8xshadowbusiness1@gmail.com>
2025-11-01 13:00:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
-mkdir -p /opt/render/project/src/videos
-# Run the bot (auto restart if it crashes)
 while true; do
-  echo "🚀 Starting bot at $(date)"
-  python3 instaautomation.py
-  echo "⚠️ Bot crashed — restarting in 10 seconds..."
-  sleep 10
+  echo "🚀 Starting InstaAutomation at $(date)"
+  python3 instaautomation_full.py
+  echo "⚠️ Bot crashed — restarting in 5 seconds..."
+  sleep 5
 done
